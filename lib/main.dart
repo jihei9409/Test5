0ae006274f40da6faa18e48184db24985faa9b6f
jihei9409 <jihei9409@naver.com>
2022-01-09 16:12:48 +0900

    [UPDATE]

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
-
 // TEST입니다.
 
 void main() {
@@ -16,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo TEST!!',
       theme: ThemeData(
         // This is the theme of your application.
         //
